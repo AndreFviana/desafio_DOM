b7b2b1d80a481f02737df45f107cb5e594c30a4e
Andre Viana <91017517+AndreFviana@users.noreply.github.com>
2024-01-14 09:52:34 -0300

    creat: página para mostrar a inserção e alteração de elementos do html usando o java script

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # desafio_DOM
 Repositorio para a elaboração do desafio de alterar o Document Object Model de uma página web usando o Java Script.
+<img src="./img/img-redme.png" alt="foto do personagem Dominic Toreto" width="300px">
---
+++ script.js
@@ -0,0 +1,23 @@
+//Título
+let elementoTitulo = document.getElementById('titulo')
+elementoTitulo.innerText = 'Document Object Model';
+//Link
+let elementoA = document.querySelector('a')
+elementoA.textContent = 'Proz Educação';
+//Adicionando elementos à lista não ordenada
+let elementosNaoOrdenados = document.querySelector('ul')
+let novoLi = document.createElement('li')
+novoLi.textContent = 'Altera o html com Java Script'
+elementosNaoOrdenados.appendChild(novoLi)
+let novoLi2 = document.createElement('li')
+novoLi2.textContent = 'Manipulação do DOM com o Java Script'
+elementosNaoOrdenados.appendChild(novoLi2)
+let novoLi3 = document.createElement('li')
+novoLi3.textContent = 'Interação'
+elementosNaoOrdenados.appendChild(novoLi3)
+//Adicionando elementos à lista ordenada
+let elementosOrdenados = document.getElementById('lista-ordenada')
+elementosOrdenados.innerHTML = `
+<li><a href="https://webflow.com/glossary/document-object-model-dom?utm_source=google&utm_medium=search&utm_campaign=SS-GoogleSearch-Nonbrand-DynamicSearchAds-Global&utm_term=dsa-1480385100845___617245336966__&gad_source=1&gclid=CjwKCAiAqY6tBhAtEiwAHeRopRxBKdCllFKJ62GLQfiego8QL0bAwWszOQ8GHf68eD45aKbqLXeGQBoCyUUQAvD_BwE">O que é DOM?</a></li>
+<li><a href="https://www.hostinger.com.br/tutoriais/dom-o-que-e">Dom é o coração do desenvolvimento web?</a></li>
+<li><a href="https://www.devmedia.com.br/trabalhando-com-dom-em-javascript/29039">DOM é a abreviação de Domic Toreto?</a></li>`
