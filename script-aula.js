433c1b131cd97f6a94b7a402c4902b5d065e91f2
Andre Viana <91017517+AndreFviana@users.noreply.github.com>
2024-03-20 23:31:14 -0300

    fix: correção na função para adicionar item no card da pagina index.aula

--- script-aula.js
+++ script-aula.js
@@ -1,15 +1,6 @@
-/*
+let valorProduto01 = 11.66;
 let quantidadeSubtotal = document.getElementById("quantidade-subtotal");
 let valorSubtotal = document.getElementById("valor-subtotal");
-
-let subtotalInfo = {
-  quantidade: 1,
-  valor: 11.66,
-};
-
-quantidadeSubtotal.innerText = subtotalInfo.quantidade + " itens";
-valorSubtotal.innerText = subtotalInfo.valor;
-
 //adicionar
 let btnAdicionar = document.querySelector('#btn-adicionar-produto-01');
 let quantidadeProduto = document.querySelector('#quantidade-produto-01');
@@ -17,16 +8,30 @@ let quantidadeProduto = document.querySelector('#quantidade-produto-01');
 //subtrair
 let btnSubtrair01 = document.querySelector('#btn-subtrair-produto-01');
 
+let subtotalInfo = {
+  quantidade: 1,
+  valor: 11.66,
+};
+
+//eventos
+quantidadeSubtotal.innerText = subtotalInfo.quantidade + " itens";
+valorSubtotal.innerText = subtotalInfo.valor 
+btnAdicionar.addEventListener('click',adicionar01)
+btnSubtrair01.addEventListener('click',subtrair01)
+
 
 //função adicionar
 function adicionar01 () {
   quantidadeProduto.value = Number (quantidadeProduto.value)+1
+  quantidadeSubtotal.innerText = subtotalInfo.quantidade + " itens";
+  subtotalInfo.quantidade = subtotalInfo.quantidade +1
 }
+
 //função subtrair
 function subtrair01() {
-  if (valorAtual > 1) {
-    qtdProduto.value = valorAtual - 1;
-    subtotalInfo.quantidade = valorAtual - 1;
+  if (valorProduto01 > 1) {
+    qtdProduto.value = valorProduto01 - 1;
+    subtotalInfo.quantidade = valorProduto01 - 1;
     subtotalInfo.valor = subtotalInfo.quantidade * 11.66;
     atualizarTotal();
   } else {
@@ -36,20 +41,16 @@ function subtrair01() {
 
 //função subTotal
 function subTotal (){
-  subtotalInfo.quantidade= subtotalInfo.quantidade+
+  subtotalInfo.quantidade= subtotalInfo.quantidade+1
   }
- 
-//evento
-btnAdicionar.addEventListener('click',adicionar01)
-btnSubtrair01.addEventListener('click',subtrair01)
+
 subTotal()
 
-function adicionarUm (){
-  quantidadeProduto01.value = Number(quantidadeProduto01.value)+1
 
-}
-*/
 
+
+
+/*
 let inputEmail = document.getElementById('email');
 let inputIdade = document.getElementById('idade');
 let formulario = document.querySelector('form');
@@ -64,7 +65,7 @@ inputEmail.addEventListener("blur", (e)=> {
 /*
 formulario.addEventListener("submit", ()=> {
 alert("Eviado com sucesso!")
-})  */
+})  
 
   //isso aqui muda o background quando a pessoa é menor ou maior de idade
 inputIdade.addEventListener("change", ()=>{
@@ -77,11 +78,15 @@ inputIdade.addEventListener("change", ()=>{
 })
 
 let modoDark = document.getElementsByClassName('.modo-dark')
-let dark = document.querySelector('dark')
+let dark = document.querySelector('.dark')
 
-function mudarBackground (){
+
+dark.addEventListener('click', function() {
   dark.style.backgroundColor = 'white';
-}
-modoDark.addEventListener("focus", mudarBackground)
+});
 
-inputIdade.classList.add('item-carrinho ')  
+
+let btn = document.querySelector('.btn')
+*/
+  
+ 
